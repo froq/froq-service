15360f88e81470a4626deec4962b48336f409415
Kerem Güneş <k-gun@mail.com>
2019-03-13 02:59:24 +0300

    Service: optimize run().

--- src/Service.php
+++ src/Service.php
@@ -554,11 +554,12 @@ abstract class Service
             self::METHOD_ON_BEFORE, self::METHOD_ON_AFTER,
         ];
 
-        @ [$serviceName, $serviceMethod] = $request->uri()->segments();
+        $serviceName = $request->uri()->segment(0);
         if ($serviceName != null && strtolower($serviceName) == strtolower($serviceMain)) {
             // redirect "main/" to "/" (301 Moved Permanently)
             return $response->redirect('/', 301)->end();
         }
+        $serviceMethod = $request->uri()->segment(1);
         if ($serviceMethod != null && strtolower($serviceMethod) == $methodMain) {
             // redirect "<service>/main" to "<service>/" (301 Moved Permanently)
             return $response->redirect('/'. strtolower($serviceName), 301)->end();
